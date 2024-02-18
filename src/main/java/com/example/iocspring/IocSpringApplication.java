8a974e7831113c02cd292d236fc94db4bad14740
hamza <hamzabenmoussa166@gmail.com>
2024-02-18 11:08:42 +0100

    Initial commit

---
+++ src/main/java/com/example/iocspring/IocSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.iocspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocSpringApplication.class, args);
+	}
+
+}
